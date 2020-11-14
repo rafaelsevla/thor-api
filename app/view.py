42cb0919fdbe1cf0348d654b57c3fc2fc3fd7708
Rafael Costa <rafaelsevla@gmail.com>
2020-11-14 00:17:36 -0300

    Add analyze request

--- app/view.py
+++ app/view.py
@@ -1,9 +1,11 @@
 import os
+from flask.globals import session
 import requests
 from app import app, db
 from datetime import datetime
 from flask import request
 from app.model import City, Forecast
+from sqlalchemy.sql import func
 
 
 @app.route('/city', methods=['GET', 'POST'])
@@ -13,7 +15,7 @@ def city():
         return { 'reason': 'Missing id param' }, 400
 
     if request.method == 'GET':
-        city = db.session.query(City).filter(City.apiadvisor_id == param_city_id).scalar()
+        city = db.session.query(City).filter_by(apiadvisor_id=param_city_id).scalar()
         if city is not None:
             return city.jsonized(), 200
         return 'Not found', 404
@@ -29,13 +31,13 @@ def city():
     forecast = requests.get(url).json()
 
     if (forecast.get('error') == True):
-        return { 'reason': forecast.get('detail') }, 400
+        return 'Not found', 404
 
     city_detail = City(
         name=forecast.get('name'),
         state=forecast.get('state'),
         country=forecast.get('country'),
-        apiadvisor_id=param_city_id
+        apiadvisor_id=int(param_city_id)
     )
     city_id = city_detail.get_or_create()
 
@@ -66,3 +68,56 @@ def city():
         'city_detail': city_detail.jsonized(),
         'data': forecasts_to_show_in_return
     }, 201   
+
+
+@app.route('/analyze', methods=['GET'])
+def analyze():
+    initial_date = request.args.get('initial_date', None)
+    if (initial_date == None):
+        return { 'reason': 'Missing initial_date param' }, 400
+
+    final_date = request.args.get('final_date', None)
+    if (final_date == None):
+        return { 'reason': 'Missing final_date param' }, 400
+
+    if (initial_date >= final_date):
+        return { 'reason': 'The initial_date must be less than the final_date' }
+
+    city_with_maximum_temperature = db.session.query(
+            Forecast.maximum_temperature.label('temperature'),
+            City.name.label('name'),
+            Forecast.date   
+        ).join(
+            Forecast,
+            City.id == Forecast.city_id
+        ).filter(
+            Forecast.date.between(initial_date, final_date)
+        ).order_by(Forecast.maximum_temperature.desc()).first()
+
+    if city_with_maximum_temperature is None:
+        return 'No data registered on that date', 404
+
+    forecasts_avg = db.session.query(
+        City.name.label('city_name'),
+        func.avg(Forecast.precipitation).label('avg_precipitation')
+    ).join(
+        Forecast,
+        City.id == Forecast.city_id
+    ).filter(
+        Forecast.date.between(initial_date, final_date)).group_by(Forecast.city_id)
+
+    forecasts_avg_to_show_in_return = []
+    for forecast_avg in forecasts_avg:
+        forecasts_avg_to_show_in_return.append({
+            'city_name': forecast_avg.city_name,
+            'avg_precipitation': forecast_avg.avg_precipitation
+        })
+
+    return {
+        'city_with_maximum_temperature': {
+            'city_name': city_with_maximum_temperature.name,
+            'temperature': city_with_maximum_temperature.temperature,
+            'date': city_with_maximum_temperature.date
+        },
+        'cities_precipitation': forecasts_avg_to_show_in_return,
+    }, 200
